106bceb38cfb03e57d73dde60fef25d7d640d0ac
youjeong-chae <yjchae16@naver.com>
2020-09-29 12:23:10 +0900

    20200929

---
+++ src/chap02/type/ShortType.java
@@ -0,0 +1,14 @@
+package chap02.type;
+
+public class ShortType {
+	public static void main(String[] args) {
+		//short : 2byte
+		short shortValue1;
+		shortValue1 = 32767;
+		System.out.println(shortValue1);
+		
+		//shortValue1 = 32768; x 
+		shortValue1 = -32768;
+		System.out.println(shortValue1);
+	}
+}
